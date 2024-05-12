71c239e14f710d99dcde08183a8f6c8bd97cf3d4
John Aziz <john.aziz@gemindz.com>
2024-05-12 18:02:57 +0300

    Remove ask and fix nav

--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -1,6 +1,4 @@
-import { Outlet, NavLink, Link } from "react-router-dom";
-
-import github from "../../assets/github.svg";
+import { Outlet, Link } from "react-router-dom";
 
 import styles from "./Layout.module.css";
 
@@ -12,32 +10,7 @@ const Layout = () => {
                     <Link to="/" className={styles.headerTitleContainer}>
                         <h3 className={styles.headerTitle}>Build 24 LangChain vCore | Sample</h3>
                     </Link>
-                    <nav>
-                        <ul className={styles.headerNavList}>
-                            <li>
-                                <NavLink to="/" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
-                                    Chat
-                                </NavLink>
-                            </li>
-                            <li className={styles.headerNavLeftMargin}>
-                                <NavLink to="/qa" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
-                                    Ask a question
-                                </NavLink>
-                            </li>
-                            <li className={styles.headerNavLeftMargin}>
-                                <a href="https://github.com/khelanmodi/build-24-langchain-vcore" target={"_blank"} title="Github repository link">
-                                    <img
-                                        src={github}
-                                        alt="Github logo"
-                                        aria-label="Link to github repository"
-                                        width="20px"
-                                        height="20px"
-                                        className={styles.githubLogo}
-                                    />
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
+                    <nav></nav>
                     <h4 className={styles.headerRightText}>Azure OpenAI + Cosmos DB for MongoDB vCore</h4>
                 </div>
             </header>
