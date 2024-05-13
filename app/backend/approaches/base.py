ea08a6c3ddffa066c94dfa2aaf7b7da18e942429
John Aziz <john.aziz@gemindz.com>
2024-05-13 22:20:29 +0300

    improve to unite types and proper usage of lc

--- app/backend/approaches/base.py
+++ app/backend/approaches/base.py
@@ -1,15 +1,18 @@
 from abc import ABC, abstractmethod
 
 from langchain_core.documents import Document
-from langchain_openai import AzureOpenAIEmbeddings
+from langchain_openai import AzureChatOpenAI, AzureOpenAIEmbeddings
 
 
 class ApproachesBase(ABC):
-    def __init__(self, connection_string: str, database_name: str, embedding: AzureOpenAIEmbeddings):
+    def __init__(
+        self, connection_string: str, database_name: str, embedding: AzureOpenAIEmbeddings, chat: AzureChatOpenAI
+    ):
         self._connection_string = connection_string
         self._database_name = database_name
         self._embedding = embedding
+        self._chat = chat
 
     @abstractmethod
-    def run(self, collection_name: str, query: str, limit: int, score_threshold: float) -> list[Document]:
+    def run(self, collection_name: str, query: str, limit: int, score_threshold: float) -> tuple[list[Document], str]:
         raise NotImplementedError
--- app/backend/approaches/vector.py
+++ app/backend/approaches/vector.py
@@ -5,9 +5,15 @@ from backend.approaches.utils import vector_store_api
 
 
 class Vector(ApproachesBase):
-    def run(self, collection_name: str, query: str, limit: int, score_threshold: float) -> list[Document]:
+    def run(self, collection_name: str, query: str, limit: int, score_threshold: float) -> tuple[list[Document], str]:
         namespace = f"{self._database_name}.{collection_name}"
         vector_store = vector_store_api(
             namespace=namespace, connection_string=self._connection_string, embedding=self._embedding
         )
-        return vector_store.similarity_search(query, k=limit, score_threshold=score_threshold)
+        retriever = vector_store.as_retriever(
+            search_type="similarity", search_kwargs={"k": limit, "score_threshold": score_threshold}
+        )
+        vector_response = retriever.invoke(query)
+        if vector_response:
+            return vector_response, vector_response[0].page_content
+        return [], ""
